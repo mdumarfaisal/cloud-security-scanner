31c0f94f18cab134cc7274c6e01341994824567d
mdumarfaisal <umarfaisal3548@gmail.com>
2026-02-24 21:49:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from scanner.iam_scanner import check_admin_policies, check_mfa_enabled
+from scanner.s3_scanner import check_public_buckets
+import json
 
-def run_iam_scan():
+def run_scan():
     findings = []
 
     findings.extend(check_admin_policies())
     findings.extend(check_mfa_enabled())
+    findings.extend(check_public_buckets())
 
     if not findings:
-        print("No IAM security issues found.")
+        print("No security issues found.")
     else:
-        print("IAM Security Findings:")
+        print("Security Findings:")
         for f in findings:
-            print(f"- User: {f['user']}")
-            print(f"  Issue: {f['issue']}")
-            print(f"  Severity: {f['severity']}")
-            print()
+            print(f)
+
+    # Save JSON report
+    with open("reports/report.json", "w") as file:
+        json.dump(findings, file, indent=4)
 
 if __name__ == "__main__":
-    run_iam_scan()
+    run_scan()
